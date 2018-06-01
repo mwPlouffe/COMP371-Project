59c0d180b49154b0c826dcf2589e76637f0bc960
mwPlouffe <mathew.plouffe@mail.mcgill.ca>
2018-05-31 20:17:59 -0400

    Initial Commit

---
+++ COMP371-Project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  COMP371-Project
+//
+//  Created by Mathew Plouffe on 2018-05-31.
+//  Copyright © 2018 Mathew Plouffe. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
